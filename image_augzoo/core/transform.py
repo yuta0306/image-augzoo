9e43e711eca0dbece122c4dd0a9d4a7cbdc74a56
yuta0306 <you-2001-3-6@ezweb.ne.jp>
2022-07-01 14:20:02 +0900

    abs

--- image_augzoo/core/transform.py
+++ image_augzoo/core/transform.py
@@ -1,14 +1,32 @@
+import warnings
 from typing import Any
 
 import torch
 
 
 class Transform:
-    def __init__(self, *args: Any, p: float = 0.5, **kwargs) -> None:
-        ...
+    def __init__(self, *args: Any, p: float = 1.0, **kwargs) -> None:
+        assert 0.0 <= p <= 1.0
+        self.p = p
 
-    def __call__(self, *args: torch.Tensor, **kwargs):
+    def apply(self, *inputs: torch.Tensor, **kwargs):
         raise NotImplementedError
+
+    def apply_batch(self, *inputs: torch.Tensor, **kwargs):
+        raise NotImplementedError
+
+    def __call__(self, *inputs: torch.Tensor, **kwargs):
+        is_batched = inputs[0].ndim == 4
+        if is_batched:
+            try:
+                return self.apply_batch(*inputs, **kwargs)
+            except NotImplementedError:
+                warnings.warn(
+                    "`apply` is called because `apply_batch` is not implemented",
+                    UserWarning,
+                )
+                return self.apply(*inputs, **kwargs)
+        return self.apply(*inputs, **kwargs)
 
     def __repr__(self) -> str:
         name = self.__class__.__name__
@@ -24,11 +42,23 @@ class Transform:
 
 
 class DualTransform(Transform):
-    def __init__(self, *args: Any, p: float = 0.5, **kwargs) -> None:
+    def __init__(self, *args: Any, p: float = 1.0, **kwargs) -> None:
         super().__init__(*args, p=p, **kwargs)
 
-    def __call__(self, *args: torch.Tensor, **kwargs):
-        raise NotImplementedError
+    def __call__(self, *inputs: torch.Tensor, **kwargs):
+        assert len(inputs) == 2
+        return super().__call__(*inputs, **kwargs)
+
+    def __repr__(self) -> str:
+        return super().__repr__()
+
+
+class MultiTransform(Transform):
+    def __init__(self, *args: Any, p: float = 1.0, **kwargs) -> None:
+        super().__init__(*args, p=p, **kwargs)
+
+    def __call__(self, *inputs: torch.Tensor, **kwargs):
+        return super().__call__(*inputs, **kwargs)
 
     def __repr__(self) -> str:
         return super().__repr__()
--- image_augzoo/image_augzoo/blend.py
+++ image_augzoo/image_augzoo/blend.py
@@ -1,12 +1,9 @@
-from typing import Tuple
-
-import numpy as np
 import torch
 
-from image_augzoo.core.transform import DualTransform
+from image_augzoo.core.transform import MultiTransform
 
 
-class Blend(DualTransform):
+class Blend(MultiTransform):
     """
     Blend
 
@@ -22,7 +19,6 @@ class Blend(DualTransform):
         p: float = 1.0,
         alpha: float = 0.6,
         rgb_range: float = 1.0,
-        channel_first: bool = True,
     ):
         """
         Parameters
@@ -40,36 +36,46 @@ class Blend(DualTransform):
             raise ValueError(
                 f"rgb_range must be 1.0 or 255.0, but {rgb_range} were given"
             )
-        self.p = p
         self.alpha = alpha
         self.rgb_range = rgb_range
-        self.channel_first = channel_first
+        super().__init__(p=p)
 
-    def __call__(self, *inputs: torch.Tensor, **kwargs) -> Tuple[torch.Tensor, ...]:
-        if self.alpha <= 0 or np.random.rand(1) >= self.p:
+    def apply(self, *inputs: torch.Tensor, **kwargs):
+        if self.alpha <= 0 or torch.rand(1) > self.p:
             return inputs
+        device = inputs[0].device
+        c = torch.empty((3, 1, 1), device=device).uniform_(0, self.rgb_range)
+        refs = (c.repeat((1, input_.size(-2), input_.size(-1))) for input_ in inputs)
 
-        is_batched = inputs[0].ndim == 4
-        if is_batched:
-            bs = inputs[0].size(0)
-            c = torch.empty((bs, 3, 1, 1), device=inputs[0].device).uniform_(
-                0, self.rgb_range
-            )
-            refs = (
-                c.repeat((1, 1, input_.size(-2), input_.size(-1))) for input_ in inputs
-            )
-        else:
-            bs = 1
-            c = torch.empty((3, 1, 1), device=inputs[0].device).uniform_(
-                0, self.rgb_range
-            )
-            refs = (
-                c.repeat((1, input_.size(-2), input_.size(-1))) for input_ in inputs
-            )
-
-        v = torch.empty(1).uniform_(self.alpha, 1)
+        v = torch.empty(1, device=device).uniform_(self.alpha, 1)
         transformed = tuple(
             v * input_ + (1 - v) * ref for (input_, ref) in zip(inputs, refs)
+        )
+
+        return transformed
+
+    def apply_batch(self, *inputs: torch.Tensor, **kwargs):
+        bs = inputs[0].size(0)
+        device = inputs[0].device
+        probs = torch.rand(bs, device=device)
+        if self.alpha <= 0 or (probs > self.p).all():
+            return inputs
+        c = torch.empty((bs, 3, 1, 1), device=device).uniform_(0, self.rgb_range)
+        refs = (c.repeat((1, 1, input_.size(-2), input_.size(-1))) for input_ in inputs)
+
+        v = torch.empty(bs, 1, 1, 1, device=device).uniform_(self.alpha, 1)
+        transformed = tuple(
+            (
+                v.repeat(1, input_.size(1), input_.size(2), input_.size(3)) * input_
+                + (1 - v).repeat(1, input_.size(1), input_.size(2), input_.size(3))
+                * ref
+            ).where(
+                (probs > self.p)
+                .view(-1, 1, 1, 1)
+                .expand(bs, input_.size(1), input_.size(2), input_.size(3)),
+                input_,
+            )
+            for input_, ref in zip(inputs, refs)
         )
 
         return transformed
