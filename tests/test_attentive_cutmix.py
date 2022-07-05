530e77a2ecb46053e5a6dd7bc37bfcb0a325db99
yuta0306 <you-2001-3-6@ezweb.ne.jp>
2022-07-05 15:47:34 +0900

    timm -> torchvisoin

--- tests/test_attentive_cutmix.py
+++ tests/test_attentive_cutmix.py
@@ -54,43 +54,39 @@ def test_attentive_cutmix_single_sr():
 
 def test_attentive_cutmix_batch():
     attentive_cutmix = AttentiveCutMix()
-    LR = [
+    images = [
         load_image(f"assets/image0{i}.jpg", (224, 224), SR=True) / 255.0
         for i in range(1, 7)
     ]
-    HR = [load_image(f"assets/image0{i}.jpg", (224, 224)) / 255.0 for i in range(1, 7)]
-    LR = torch.stack(LR)
-    HR = torch.stack(HR)
+    images = torch.stack(images)
 
-    processed = attentive_cutmix(LR, HR)
+    processed = attentive_cutmix(images)
     assert isinstance(processed, tuple)
-    assert len(processed) == 2
+    assert len(processed) == 1
     assert isinstance(processed[0], torch.Tensor)
-    assert LR.size() == processed[0].size()
+    assert images.size() == processed[0].size()
 
     tiles = []
-    for i in range(len(LR)):
-        tiles.extend([LR[i], HR[i], processed[0][i], processed[1][i]])
+    for i in range(len(images)):
+        tiles.extend([images[i], processed[0][i]])
     save_image(os.path.join(save_to, "attentive_cutmix_batch.png"), 4, 1.0, *tiles)
 
 
 def test_attentive_cutmix_batch_p():
     attentive_cutmix = AttentiveCutMix(p=0.2)
-    LR = [
+    images = [
         load_image(f"assets/image0{i}.jpg", (224, 224), SR=True) / 255.0
         for i in range(1, 7)
     ]
-    HR = [load_image(f"assets/image0{i}.jpg", (224, 224)) / 255.0 for i in range(1, 7)]
-    LR = torch.stack(LR)
-    HR = torch.stack(HR)
+    images = torch.stack(images)
 
-    processed = attentive_cutmix(LR, HR)
+    processed = attentive_cutmix(images)
     assert isinstance(processed, tuple)
-    assert len(processed) == 2
+    assert len(processed) == 1
     assert isinstance(processed[0], torch.Tensor)
-    assert LR.size() == processed[0].size()
+    assert images.size() == processed[0].size()
 
     tiles = []
-    for i in range(len(LR)):
-        tiles.extend([LR[i], HR[i], processed[0][i], processed[1][i]])
+    for i in range(len(images)):
+        tiles.extend([images[i], processed[0][i]])
     save_image(os.path.join(save_to, "attentive_cutmix_batch_p.png"), 4, 1.0, *tiles)
